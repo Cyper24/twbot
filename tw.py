acab77e46faa52b0bc29c0e89ed945c8c56ae505
Cyper24 <36534858+Cyper24@users.noreply.github.com>
2022-11-07 15:25:37 +0700

    Update tw.py

--- tw.py
+++ tw.py
@@ -2,10 +2,10 @@ import tweepy
 from time import sleep
 
 cl = tweepy.Client(
-    consumer_key="FvwIRu0r9h2iEPMFfUKnIlDlM",
-    consumer_secret="0wFHYx1ZNvrlfWuHDuJMLh4oFBy2JsJpAzLNcT1diU8uqw2v1b",
-    access_token="1382180529563344897-FD6HlNZw64flgvK8W8sQTXeOr2ZarU",
-    access_token_secret="dgIrQPWQ5vJAvIi2liPLfRBrSZTlJGBC6Cpz2Azw2s8uX"
+    consumer_key=" ",
+    consumer_secret=" ",
+    access_token=" ",
+    access_token_secret=" "
 )
 
 f = open("list.txt", 'r')
